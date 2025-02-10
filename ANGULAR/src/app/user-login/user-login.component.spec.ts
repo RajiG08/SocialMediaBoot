98f7bb1441f228d1b53967ab86d7c6f6f6812fe2
Rajalakshmi G <66936135+RajiG08@users.noreply.github.com>
2025-02-10 14:24:40 +0530

    Create user-login.component.spec.ts

---
+++ ANGULAR/src/app/user-login/user-login.component.spec.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
+})
+export class UserLoginComponent {
+  
+}
